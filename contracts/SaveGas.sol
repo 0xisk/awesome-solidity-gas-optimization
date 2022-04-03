50bcd51b6c64c796636906fe5b770ba1102b4ad9
nonfungibleayo-eth <name.can.be.blank.eth@gmail.com>
2022-04-03 11:02:51 +0000

    rename variables

--- contracts/SaveGas.sol
+++ contracts/SaveGas.sol
@@ -2,22 +2,22 @@ pragma solidity ^0.8.1;
 
 // Source: https://marduc812.com/2021/04/08/how-to-save-gas-in-your-ethereum-smart-contracts/
 contract SaveGas {
-    uint8 resulta = 0;
-    uint256 resultb = 0;
+    uint8 resultA = 0;
+    uint256 resultB = 0;
 
     function UseUint() external returns (uint256) {
         uint256 selectedRange = 50;
         for (uint256 i = 0; i < selectedRange; i++) {
-            resultb += 1;
+            resultB += 1;
         }
-        return resultb;
+        return resultB;
     }
 
     function UseUInt8() external returns (uint8) {
         uint8 selectedRange = 50;
         for (uint8 i = 0; i < selectedRange; i++) {
-            resulta += 1;
+            resultA += 1;
         }
-        return resulta;
+        return resultA;
     }
 }
